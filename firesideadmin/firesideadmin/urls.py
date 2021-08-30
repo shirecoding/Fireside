54cf926169c20558085ec89ed308ab6c98219c43
benjamin <benjaminhon86@gmail.com>
2021-08-30 23:52:47 +0800

    Add authentication views and templates

--- firesideadmin/firesideadmin/urls.py
+++ firesideadmin/firesideadmin/urls.py
@@ -16,4 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("polls/", include("polls.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("polls/", include("polls.urls")),
+    path("", include("authentication.urls")),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    # Includes the following:
+    #
+    #   accounts/login/ [name='login']
+    #   accounts/logout/ [name='logout']
+    #   accounts/password_change/ [name='password_change']
+    #   accounts/password_change/done/ [name='password_change_done']
+    #   accounts/password_reset/ [name='password_reset']
+    #   accounts/password_reset/done/ [name='password_reset_done']
+    #   accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+    #   accounts/reset/done/ [name='password_reset_complete']
+]
