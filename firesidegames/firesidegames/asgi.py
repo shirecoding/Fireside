bed9e7aafa752cdc89786edeab22cb0c13d4b77f
benjamin <benjaminhon86@gmail.com>
2021-10-30 14:28:33 +0800

    Rename project to firesidegames

--- firesidegames/firesidegames/asgi.py
+++ firesidegames/firesidegames/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for firesideweb project.
+ASGI config for firesidegames project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firesideweb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firesidegames.settings")
 
 application = get_asgi_application()
--- firesidegames/home/apps.py
+++ firesidegames/home/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 
 
-class AuthConfig(AppConfig):
+class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "authentication"
+    name = "home"
 
     def ready(self):
         pass
