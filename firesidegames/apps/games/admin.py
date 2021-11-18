aaecda1ffe074dd55cc5177ea1b8b2ecedc625b8
benjamin <benjaminhon86@gmail.com>
2021-11-18 22:33:49 +0800

    Create GameInstance model and admin

--- firesidegames/apps/games/admin.py
+++ firesidegames/apps/games/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Game, Category
+from .models import Game, Category, GameInstance
 
-admin.site.register(Game)
+
+class GameInstanceInline(admin.TabularInline):
+    """
+    When creating a new instance from inline, a default random uid for the instance is created,
+    slightly modify it so that django detects a change, else it will no create a new instance
+    """
+
+    model = GameInstance
+    extra = 0
+
+
+class GameAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Game", {"fields": ["name", "short_description", "long_description"]}),
+        ("Properties", {"fields": ["categories", "image"]}),
+    ]
+    inlines = [
+        GameInstanceInline,
+    ]
+
+
+admin.site.register(Game, GameAdmin)
 admin.site.register(Category)
+admin.site.register(GameInstance)
--- firesidegames/apps/games/models.py
+++ firesidegames/apps/games/models.py
@@ -1,19 +1,36 @@
 from django.db import models
+import uuid
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=64, unique=True)
 
     def __str__(self):
+        return self.name
+
+    @property
+    def uid(self):
         return self.name
 
 
 class Game(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=64, unique=True)
     categories = models.ManyToManyField(Category)
-    short_description = models.CharField(max_length=50)
-    long_description = models.TextField(max_length=500)
+    short_description = models.CharField(max_length=64)
+    long_description = models.TextField(max_length=4096)
     image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def uid(self):
+        return self.name
+
+
+class GameInstance(models.Model):
+    uid = models.CharField(default=uuid.uuid4, max_length=64, unique=True)
+    game = models.ForeignKey(Game, related_name="instances", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.game.name}_{self.uid}"
