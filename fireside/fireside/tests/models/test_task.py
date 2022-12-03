aaa23ee9072b0d2b773bfbbe6c6e08767823d009
benjaminhon <benjaminhon86@gmail.com>
2022-12-03 22:44:06 +0800

    Refactor task tests

--- fireside/fireside/tests/models/test_task.py
+++ fireside/fireside/tests/models/test_task.py
@@ -1,39 +1,45 @@
 import pytest
-from fireside_tests.models import BasicShipModel
 from fireside.models import Task
 from fireside.models import TaskSchedule
 from fireside.models import TaskPriority
 from django_rq import get_scheduler
+from fireside.utils import function_to_import_path
+
+
+def dummy_task(*args, **kwargs):
+    pass
 
 
 @pytest.fixture
-def task(db) -> None:
-    import fireside_tests.tasks  # register the repair ship task
+def task(db) -> Task:
+    return Task.objects.create(
+        name="Dummy Task",
+        description="Dummy Task",
+        fpath=function_to_import_path(dummy_task),
+    )
 
 
 @pytest.fixture
-def bsg(db) -> BasicShipModel:
-    obj = BasicShipModel.objects.create(name="Battle Star Galactica")
-    return obj
+def task_schedule(db, task) -> TaskSchedule:
 
-
-def test_task(bsg, task):
-    # test create task definition
-    task = Task.objects.get(name="Repair Ship")
-    assert task.name == "Repair Ship"
-
-    # test create task
     task_schedule = TaskSchedule.objects.create(
         task=task,
         cron="* * * * *",
         repeat=2,
         priority=TaskPriority.LOW,
-        inputs={
-            "args": str(bsg.uid),
-            "kwargs": {},
-        },
+        inputs={},
     )
     assert TaskSchedule.objects.get(task=task).task == task
+
+    return task_schedule
+
+
+def task_task_decorator():
+    task = Task.objects.get(name="Healthcheck")
+    assert task.name == "Healthcheck"
+
+
+def test_task_schedule(task_schedule):
 
     # test queue
     assert task_schedule.get_queue().name == TaskPriority.LOW
